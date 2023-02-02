d24a136fcb398cb713bfa66b9a4657cc77510889
Francisco Tomás <camelocomsede1@gmail.com>
2023-02-02 19:49:49 +0000

    placeholder backend for the file architecture in aws

---
+++ main.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+const axios = require("axios")
+
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (request, response) => {
+  axios({
+    method: "get",
+    url: "https://dummyjson.com/todos/random",
+  })
+    .then((res) => {
+      return response.status(200).json(res.data)
+    })
+    .catch((err) => {
+      return response.status(500).json({ err })
+    })
+})
+
+// server up
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}...`)
+})
